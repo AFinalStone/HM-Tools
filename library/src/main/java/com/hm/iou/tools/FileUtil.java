a9f376fe435fe81d68d0fdff17337cc9bbd3510b
shiyaolei <shiyaolei@dafy.com>
2018-05-25 20:24:26 +0800

    更新工具类FileUtil

--- library/src/main/java/com/hm/iou/tools/FileUtil.java
+++ library/src/main/java/com/hm/iou/tools/FileUtil.java
@@ -1,7 +1,11 @@
 package com.hm.iou.tools;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Environment;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * 文件存储工具类
@@ -13,9 +17,10 @@ import android.os.Environment;
 public class FileUtil {
 
     /**
+     *
      * getCacheDir()方法用于获取/data/data/<application package>/cache目录
      * getFilesDir()方法用于获取/data/data/<application package>/files目录
-     *
+     * <p>
      * 应用程序在运行的过程中如果需要向手机上保存数据，一般是把数据保存在SDcard中的。
      * 大部分应用是直接在SDCard的根目录下创建一个文件夹，然后把数据保存在该文件夹中。
      * 这样当该应用被卸载后，这些数据还保留在SDCard中，留下了垃圾数据。
@@ -23,18 +28,51 @@ public class FileUtil {
      * 通过Context.getExternalFilesDir()方法可以获取到 SDCard/Android/data/你的应用的包名/files/ 目录，一般放一些长时间保存的数据
      * 通过Context.getExternalCacheDir()方法可以获取到 SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
      * 如果使用上面的方法，当你的应用在被用户卸载后，SDCard/Android/data/你的应用的包名/ 这个目录下的所有文件都会被删除，不会留下垃圾信息。
+     */
+
+
+    /**
+     * 获取外部SD卡的缓存路径，需要访问外部存储设备的权限
      *
      * @param context
      * @return
      */
-    public static String getDiskCacheDir(Context context) {
+    public static String getExternalCacheDirPath(Context context) {
         String cachePath = null;
         if (hasSdcard()) {
             cachePath = context.getExternalCacheDir().getPath();
-        } else {
-            cachePath = context.getCacheDir().getPath();
         }
         return cachePath;
+    }
+
+    /**
+     * 获取APP本身的缓存路径
+     *
+     * @param context
+     * @return
+     */
+    public static String getCacheDirPath(Context context) {
+        return context.getCacheDir().getPath();
+    }
+
+    /**
+     * 保存Bitmap图片到目标文件
+     *
+     * @param bitmap
+     * @param target
+     * @return
+     */
+    public static boolean saveBitmapToTargetFile(Bitmap bitmap, File target) {
+        try {
+            FileOutputStream out = new FileOutputStream(target);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+            out.flush();
+            out.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     private static boolean hasSdcard() {
