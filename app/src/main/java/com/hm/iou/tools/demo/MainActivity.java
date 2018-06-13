d6bc03d2cadd708d2b68479634128fbab45dab40
houjinyun <houjinyun@gmail.com>
2018-06-13 11:38:02 +0800

    增加金额大写工具类

--- app/src/main/java/com/hm/iou/tools/demo/MainActivity.java
+++ app/src/main/java/com/hm/iou/tools/demo/MainActivity.java
@@ -1,12 +1,17 @@
 package com.hm.iou.tools.demo;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
+import com.hm.iou.tools.MoneyFormatUtil;
 import com.hm.iou.tools.ToastUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEtAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ToastUtil.showStatusView(MainActivity.this, "加载成功", true);
+            }
+        });
+
+        mEtAmount = findViewById(R.id.et_test_upper);
+        findViewById(R.id.btn_test3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String amount = mEtAmount.getText().toString();
+                    String upper = MoneyFormatUtil.toChineseCharI(amount);
+                    System.out.println(upper);
+                    ToastUtil.showMessage(MainActivity.this, upper);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
