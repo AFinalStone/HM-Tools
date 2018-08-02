090a83a88bf245f526f1e74a7a2e259c54374c0b
shilei <shiyaolei@dafy.com>
2018-08-02 15:58:05 +0800

    添加获取屏幕宽和高的工具类

---
+++ library/src/main/java/com/hm/iou/tools/ScreenUtil.java
@@ -0,0 +1,89 @@
+package com.hm.iou.tools;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by syl on 2018/8/2.
+ */
+
+public class ScreenUtil {
+
+    /**
+     * 获取屏幕宽度
+     *
+     * @return
+     */
+    public static int getScreenWidth(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics dm = new DisplayMetrics();
+        display.getMetrics(dm);
+        int width = dm.widthPixels;
+        return width;
+    }
+
+    /**
+     * 获取屏幕高度，但是不包括虚拟功能高度
+     *
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics dm = new DisplayMetrics();
+        display.getMetrics(dm);
+        int height = dm.heightPixels;
+        return height;
+    }
+
+    /**
+     * 通过反射，获取包含虚拟键的整体屏幕高度
+     *
+     * @return
+     */
+    public static int getScreenRealHeight(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics dm = new DisplayMetrics();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            display.getRealMetrics(dm);
+        } else {
+            display.getMetrics(dm);
+        }
+        int realHeight = dm.heightPixels;
+        return realHeight;
+    }
+
+    /**
+     * 获取底部导航栏高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getNavigationHeight(Context context) {
+        int navigationHeight = 0;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics dm = new DisplayMetrics();
+        try {
+            @SuppressWarnings("rawtypes")
+            Class c = Class.forName("android.view.Display");
+            @SuppressWarnings("unchecked")
+            Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+            method.invoke(display, dm);
+            navigationHeight = dm.heightPixels - display.getHeight();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return navigationHeight;
+    }
+
+
+}
