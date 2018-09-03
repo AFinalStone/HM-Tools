30a67d81c2b2fb84610e14bb0003f895582c65f1
shilei <shiyaolei@dafy.com>
2018-09-03 18:23:49 +0800

    修改KeyboardUtil工具类

--- library/src/main/java/com/hm/iou/tools/KeyboardUtil.java
+++ library/src/main/java/com/hm/iou/tools/KeyboardUtil.java
@@ -2,6 +2,7 @@ package com.hm.iou.tools;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 import java.util.Timer;
@@ -14,7 +15,7 @@ import java.util.TimerTask;
 public class KeyboardUtil {
 
     /**
-     * 打开软键盘
+     * 显示软键盘的状态
      */
     public static void openKeyboard(final Activity activity) {
 
@@ -22,14 +23,31 @@ public class KeyboardUtil {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                InputMethodManager imm = (InputMethodManager) activity.getApplication().getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+                try {
+                    InputMethodManager imm = (InputMethodManager) activity.getApplication().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInputFromInputMethod(activity.getWindow().getDecorView().getWindowToken(), 0);
+                } catch (Exception e) {
+
+                }
             }
         }, 300);
     }
 
     /**
-     * 关闭键盘
+     * 显示键盘
+     */
+    public static void showKeyboard(final View view) {
+        view.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                InputMethodManager imm = (InputMethodManager) view.getContext().getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(view, InputMethodManager.SHOW_FORCED);
+            }
+        }, 300);
+    }
+
+    /**
+     * 隐藏键盘
      *
      * @param activity
      * @return
