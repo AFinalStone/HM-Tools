b4cfd1d52ef499118e76cf1d5e66582f56e1e1d1
houjinyun <houjinyun@gmail.com>
2018-11-01 17:27:29 +0800

    修改获取状态栏高度方法

--- library/src/main/java/com/hm/iou/tools/StatusBarUtil.java
+++ library/src/main/java/com/hm/iou/tools/StatusBarUtil.java
@@ -5,6 +5,8 @@ import android.graphics.Rect;
 import android.view.View;
 import android.view.Window;
 
+import java.lang.reflect.Field;
+
 /**
  * Created by syl on 2018/10/26.
  */
@@ -12,12 +14,38 @@ import android.view.Window;
 public class StatusBarUtil {
 
     public static int getStatusBarHeight(Context context) {
+        int h = getStatusBar1(context);
+        if (h == 0) {
+            h = getStatusBar2(context);
+        }
+        return h;
+    }
+
+    private static int getStatusBar1(Context context){
         int result = 0;
-        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = context.getResources().getDimensionPixelSize(resourceId);
+        try {
+            int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                result = context.getResources().getDimensionPixelSize(resourceId);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return result;
+    }
+
+    private static int getStatusBar2(Context context) {
+        try {
+            Class c = Class.forName("com.android.internal.R$dimen");
+            Object obj = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = Integer.parseInt(field.get(obj).toString());
+            int statusBarHeight = context.getResources().getDimensionPixelSize(x);
+            return statusBarHeight;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     public static int getStatusBarHeight(Window window) {
