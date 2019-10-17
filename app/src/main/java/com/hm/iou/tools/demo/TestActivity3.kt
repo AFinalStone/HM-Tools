2903b3bd6a37f79e704baab222eb387af7aa226e
石磊 <shilei@shileideMacBook-Pro.local>
2019-10-17 14:28:39 +0800

    add: 修改demo

---
+++ app/src/main/java/com/hm/iou/tools/demo/TestActivity3.kt
@@ -0,0 +1,12 @@
+package com.hm.iou.tools.demo
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author : 借条管家-shilei
+ * @version : 0.0.1
+ * @Date : 2019-10-17 14:20
+ * @E-Mail : afinalstone@foxmail.com
+ */
+class TestActivity3 : AppCompatActivity() {
+}
